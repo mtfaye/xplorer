7b2fdf38f4c9f619ca1c106b83fba0249bb29372
mtfaye <mouhameth.faye@resolutioninc.ca>
2020-07-06 14:43:44 -0400

    initialized unittesting for utils

---
+++ tests/test_utilities.py
@@ -0,0 +1,24 @@
+import unittest
+
+from src.utils import (_corr, _encoder, cat, df, duplicates, log_num, num,
+                       processed_df, read_sql, sampling, simple_corr, stats,
+                       summary, toExcel)
+
+Class TestUtils(unittest.TestCase):
+    def test_df(self):
+        pass
+    def test_catdf(self):
+        pass    
+    def test_numdf(self):
+        pass
+    def test_duplicateDf(self):
+        pass 
+    def test_encoderofDf(self):
+        pass
+if __name__ == "__main__":
+    unittest.main()
+    
+    
+    
+    
+        
