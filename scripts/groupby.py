95e6f44d948f175555a5a0061b326288bc47d28a
mtfaye <mouhameth.faye@resolutioninc.ca>
2020-07-17 13:25:13 -0400

    completed non unique value

--- scripts/groupby.py
+++ scripts/groupby.py
@@ -1,5 +1,12 @@
 import pandas as pd 
 
+df = pd.DataFrame(dict(A=['coke', 'sprite', 'coke', 'sprite',
+                          'sprite', 'coke', 'coke'],
+                       B=['alpha','gamma', 'alpha', 'beta',
+                          'gamma', 'beta', 'beta'],
+                       col_1=[1,2,3,4,5,6,7],
+                       col_2=[1,6,2,4,7,9,3]))
+
 def groupbyHelper(df, v, x, y, z):
     """ Calculates 2x2 groupby functions of four given column.
     Args:
@@ -7,29 +14,8 @@ def groupbyHelper(df, v, x, y, z):
     Returns:
     Dataframe
     """
-    if v == None:  
-        df[x].groupby([y, z]).mean()
-    elif v and y == None:
-        df[x].groupby(z).mean()
-    elif v and z == None:
-        df[x].groupby(y).mean()
-    elif x == None
-        df[v].groupby([y, z]).mean()
-    elif x and y == None:
-        df[v].groupby(z).mean()
-    elif x and z == None:
-        df[v].groupby(y).mean()
-    elif x == None:
-        df[v].groupby([y, z]).mean()
-    elif y == None:
-        df[v, x].groupby(z).mean()
-    elif z == None:
-        df[[v, x]].groupby(y).mean()
-    else:
-        df_groupby = df[[v, x]].groupby([y, z]).mean()
-        
+    df_groupby = df[[v, x]].groupby([y, z]).mean()
     return df_groupby
-        
-        
-        
+
+groupbyHelper(df, 'A', 'B', 'col_1', 'col_2')
 
